1f90826c3a6dd196647a50c15ebeab7aac69deaa
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-26 19:55:06 -1000

    beginning to work on controls

--- js/delta.js
+++ js/delta.js
@@ -5,28 +5,35 @@ function Timer () {
   EventEmitter.call(this);
   var self = this;
   this.i = 0;
-  setInterval(function () {
-    self.emit('tick', { interval : self.i++ });
-  }, 1000);
+
+  this.start = setInterval(function () {
+                self.emit('tick', { interval : self.i++ });
+              }, 1000);
 }
+
 util.inherits(Timer, EventEmitter);
 
 var myTimer = new Timer();
 
-function tickHandler(event){
+
+
+function theTicker(event){
   process.stdout.write('tick ' + this.i + '\n');
-  // if(this.i == 5){
-  //   this.removeListener('tick', tickHandler);
-  // }
+  process.stdout.write('tick ' + Date.now() + '\n');
 }
-myTimer.addListener('tick', tickHandler);
+
+myTimer.on('tick', theTicker);
 
 
 module.exports = {
   eventEmitter : EventEmitter,
   Timer : Timer,
-  tickHandler : tickHandler,
+  theTicker : theTicker,
   myTimer : myTimer
 
 };
 
+// below puts a limit on the amount of ticks
+  // if(this.i == 5){
+  //   this.removeListener('tick', tickHandler);
+  // }
