0650ac5877ab876081308a25bb1d348808fcde87
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-26 19:22:27 -1000

    basic timer working

--- js/delta.js
+++ js/delta.js
@@ -1,7 +1,32 @@
-var delta;
+var EventEmitter = require('events');
+var util = require('util');
+
+function Timer () {
+  EventEmitter.call(this);
+  var self = this;
+  this.i = 0;
+  setInterval(function () {
+    self.emit('tick', { interval : self.i++ });
+  }, 1000);
+}
+util.inherits(Timer, EventEmitter);
+
+var myTimer = new Timer();
+
+function tickHandler(event){
+  process.stdout.write('tick ' + this.i + '\n');
+  // if(this.i == 5){
+  //   this.removeListener('tick', tickHandler);
+  // }
+}
+myTimer.addListener('tick', tickHandler);
 
 
+module.exports = {
+  eventEmitter : EventEmitter,
+  Timer : Timer,
+  tickHandler : tickHandler,
+  myTimer : myTimer
 
-
-module.exports = delta;
+};
 
--- test/delta-spec.js
+++ test/delta-spec.js
@@ -3,12 +3,32 @@ var chai = require("chai");
 var should = chai.should();
 var expect = chai.expect;
 
-var delta = require('../js/delta.js');
+// {
+//   timer : Timer,
+//   tickHandler : tickHandler
+// };
+
+
+var incomingObj = require('../js/delta.js');
+
+
 
 describe('Delta Force', function () {
-  describe('Basic Timer', function () {
-    it('should be a function', function () {
-      delta.should.be.a.function;
+
+  describe('Define the Timer Class', function () {
+    it('Timer should be a function', function () {
+      incomingObj.Timer.should.be.a.function;
     });
   });
+
+  describe('Basic Timer', function () {
+    it('should be a function', function () {
+      incomingObj.tickHandler.should.be.a.function;
+    });
+    it('should be an instance of Timer', function () {
+      incomingObj.myTimer.should.be.an.instanceof(incomingObj.eventEmitter);
+    });
+  });
+
+
 });
