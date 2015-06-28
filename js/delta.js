1fcbd7ba236e851b4c4dc788588cb7801930f667
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-27 20:07:39 -1000

    refactor complete - need to correct tests

--- js/delta.js
+++ js/delta.js
@@ -3,72 +3,46 @@ var util = require('util');
 
 function Timer () {
   EventEmitter.call(this);
-  var self = this;
-  this.i = 0;
-  this.currTime;
-  setInterval(function () {
-    self.emit('tick', { interval : self.i++ });
-  }, 1000);
-
-  this.start = function(){
-    intervalId =  setInterval(function () {
-      currTime = Date.now();
-      self.emit('start', currTime);
-    }, 1000);
-  }
-
-  this.stop = function(){
-    currTime = Date.now();
-    self.emit('stop', currTime)
-    clearInterval(intervalId)
-  }
-
 }
 
 util.inherits(Timer, EventEmitter);
 
+Timer.prototype.start = function() {
+  var self = this;
+
+  intervalId =  setInterval(function () {
+  startTime = Date.now();
+  self.emit('start', {startTime : startTime});
+  }, 1000);
+
+};
+
+Timer.prototype.stopNow = function() {
+  endTime = Date.now();
+  myTimer.emit('stop', {endTime : endTime});
+  clearInterval(intervalId);
+
+};
+
+
+
 var myTimer = new Timer();
 
 function tickHandler(event){
-  process.stdout.write('tick ' + this.i + '\n');
+  console.log(event);
 }
 
 myTimer.addListener('tick', tickHandler);
+myTimer.addListener('start', tickHandler);
+myTimer.addListener('stop', tickHandler);
 
+myTimer.start();
 
-//Controls section
-
-var myController = new Timer();
-
-function turnOn (event){
-  process.stdout.write('start ' + currTime + '\n');
-}
-
-myController.addListener('start', turnOn);
-myController.start();
-
-function turnOff(event){
-  process.stdout.write('stop ' + currTime + '\n');
-}
-myController.addListener('stop', turnOff);
-
-setTimeout(myController.stop, 5000);
-
-
-
-
+var goodBye = setTimeout(myTimer.stopNow, 2000);
 
 module.exports = {
   eventEmitter : EventEmitter,
   Timer : Timer,
   tickHandler : tickHandler,
-  myTimer : myTimer,
-  myController : myController,
-  turnOn : turnOn,
-  turnOff : turnOff
+  myTimer : myTimer
 };
-
-// below puts a limit on the amount of ticks
-  // if(this.i == 5){
-  //   this.removeListener('tick', tickHandler);
-  // }
--- test/delta-spec.js
+++ test/delta-spec.js
@@ -17,69 +17,38 @@ var incomingObj = require('../js/delta.js');
 describe('Delta Force', function () {
 
   describe('Define the Timer Class', function () {
-
-    var testTimer;
-    beforeEach(function () {
-      testTimer = new incomingObj.Timer();
-    });
-
     it('Timer should be a function', function () {
       incomingObj.Timer.should.be.a.function;
-    });
-    it('should have a start method', function () {
-      testTimer.start.should.be.a.function;
-    });
-
-    it('should have a stop method', function () {
-      testTimer.stop.should.be.a.function;
     });
   });
 
   describe('Basic Timer', function () {
 
-    var testTimer;
     beforeEach(function () {
-      testTimer = new incomingObj.Timer();
+      timer = new incomingObj.Timer;
     });
 
-    it('tickhandler should be a function', function () {
-      incomingObj.tickHandler.should.be.a.function;
+    it('should be a function', function () {
+      timer.should.be.a.function;
     });
-    it('myTimer should be an instance of Timer', function () {
-      incomingObj.myTimer.should.be.an.instanceof(incomingObj.eventEmitter);
+    it('should be an instance of Timer', function () {
+      timer.should.be.an.instanceof(incomingObj.eventEmitter);
     });
-
-    // it('should emit a tick', function () {
-
-    //   var myTimer = new Timer();
-
-    //   function tickHandler(event){
-    //     process.stdout.write('tick ' + this.i + '\n');
-    //   }
-
-    //   myTimer.addListener('tick', tickHandler);
-
-    //   expect(function(){ myTimer('tick') }).to.cause(tickHandler).to.emit('tick');
-    // });
+    it('should release a tick', function () {
+      expect(function(){ timer.emit('tick') }).to.cause(timer).to.emit('tick')
+    });
   });
 
   describe('Controls', function () {
-    it('myController should be an instance of Timer', function () {
-      incomingObj.myController.should.be.an.instanceof(incomingObj.eventEmitter);
-    });
-    it('turnOn should be a function', function () {
-      incomingObj.turnOn.should.be.a.function;
+
+    beforeEach(function () {
+      timer = new incomingObj.Timer;
     });
 
-    it.skip('turnOn should emit a start', function () {
-      console.log('dasf',incomingObj.myTimer.i)
-      expect(function(){ incomingObj.myTimer.emit('tick') }).to.cause(incomingObj.Timer).to.emit('tick')
-     // expect(function(){ emitter.emit('test') }).to.cause(emitter).to.emit('test')
+    it('should release', function () {
+      expect(function(){ timer.emit('start') }).to.cause(timer).to.emit('start')
     });
 
-    it('turnOff should be a function', function () {
-      incomingObj.turnOff.should.be.a.function;
-    });
   });
 
 
