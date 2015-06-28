1eb13ea98bda0624e7bb634ba0543a42954f96ff
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-27 14:21:42 -1000

    no tests, Lag time is working

--- js/delta.js
+++ js/delta.js
@@ -11,20 +11,32 @@ function Timer () {
   //   self.emit('tick', { interval : self.i++ });
   // }, 1000);
 
-  this.start = function(num){
-    var counter = 0;
+  this.start = function(num, maxLag){
+    var numCounter = 0;
     num = num || 10;
+    maxLag = maxLag || 50;
     currTime = Date.now();
 
     intervalId =  setInterval(function () {
-      counter++;
+      numCounter++;
+
       self.emit('tick', {startTime : currTime});
 
-      if(counter > num){
+      if(numCounter > num){
         completeTime = Date.now();
         clearInterval(intervalId)
         self.emit('complete', {completeTime : completeTime})
       }
+
+      var lag = (Date.now()-((numCounter*1000)+currTime))
+        console.log('predict time', (numCounter*1000)+currTime);
+        console.log('actual  time', Date.now());
+      if( lag > maxLag  ){
+
+        self.emit('lag', { lagTime : lag})
+
+      }
+
 
     }, 1000);
 
@@ -45,12 +57,14 @@ var myTimer = new Timer();
 function tickHandler(event){
   console.log(event);
 }
+
 myTimer.addListener('tick', tickHandler);
 myTimer.addListener('complete', tickHandler);
 myTimer.addListener('stop', tickHandler);
+myTimer.addListener('lag', tickHandler);
 
 
-myTimer.start();
+myTimer.start(12,1);
 //setTimeout(myTimer.stop, 5000);
 
 
